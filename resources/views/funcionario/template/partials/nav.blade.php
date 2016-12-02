6e423c0e0773555dca721e93977d3ea394bdd798
gorkylazcano <gorky.lazcano@ceinf.cl>
2016-12-01 23:25:00 -0400

    arreglando nav de funcionario

--- resources/views/funcionario/template/partials/nav.blade.php
+++ resources/views/funcionario/template/partials/nav.blade.php
@@ -4,7 +4,7 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header" >
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
+        <span class="sr-only"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -15,18 +15,18 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" align="center">
       <ul class="nav navbar-nav">
-        <li><a href="{{route('funcionario.users.index') }}">Listado de Usuarios <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="{{route('funcionario.users.index') }}">Listado de Usuarios<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Opciones <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
+            <li><a href="{{ route('funcionario.users.create') }}">Registrar Nuevo Usuario</a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+            <li><a href="{{ route('funcionario.users.edit', Auth::user()->id) }}">Editar perfil</a></li>
           </ul>
         </li>
       </ul>
@@ -37,17 +37,21 @@
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
+        <li><a href="{{ route('funcionario.home') }}">Home</a></li>
+        @if (Auth::guest())
+            <li><a href="{{ url('/login') }}">Ingresar</a></li>
+            <!--<li><a href="{{ url('/register') }}">Crear Cuenta</a></li>-->
+        @else
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Cerrar Sesion</a></li>
+                </ul>
+            </li>
+        @endif
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
--- resources/views/funcionario/users/index.blade.php
+++ resources/views/funcionario/users/index.blade.php
@@ -1,3 +1,41 @@
 @extends('funcionario.template.main')
 
 
+@section('title','Lista de Usuarios')
+@section('content')
+<a href="{{ route('admin.users.create') }}" class="btn btn-info">Registrar nuevo Usuario</a><br><br><br>
+	<table class="table table-striped">
+		<thead>
+			<th>ID</th>
+			<th>Nombre</th>
+			<th>Email</th>
+			<th>Tipo</th>
+			<th>Acción</th>
+		</thead>
+		<tbody>
+			@foreach($users as $user)
+				<tr>
+					<td>{{ $user->id }}</td>
+					<td>{{ $user->name }}</td>
+					<td>{{ $user->email }}</td>
+					<td>
+						@if($user->type_id == "4")
+							<span class="label label-primary">Alumno</span>
+						@else
+							<span class="label label-success">Tipo de Prueba</span>
+						@endif
+					</td>
+					<td>
+						 <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-warning" title="Editar"><span class="glyphicon glyphicon-pencil" aria-hidden="true" title="Editar"></span></a>
+						 @if(Auth::user()->id != $user->id)
+								<a href="{{ route('admin.users.destroy', $user->id) }}" onclick="return confirm('¿Seguro que deseas eliminarlo?')" class="btn btn-danger" title="Eliminar"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true" title="Eliminar"></span></a>
+						@endif
+						<a href="{{ route('admin.users.detalle', $user->id) }}" class="btn btn-success" title="Detalles"><span class="glyphicon glyphicon-eye-open" aria-hidden="true" title="Detalles"></span></a>
+
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+	{!! $users->render() !!}
+@endsection
