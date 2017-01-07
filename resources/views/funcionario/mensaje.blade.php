fc9ea5a145b7633db0fc0f3d478f402a030f9045
Gorky <gorky.lazcano@ceinf.cl>
2017-01-07 11:06:48 -0400

    Arreglo correo notificacion

--- resources/views/funcionario/mensaje.blade.php
+++ resources/views/funcionario/mensaje.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
-<html lang="es">
-   <head>
-      <meta charset="utf-8">
-   </head>
-   <body>
-      <h1>Su bicicleta ha sido retirada del establecimiento. Si no ha sido usted, favor acercarse a portería</h1>
-   </body>
+<html lang="en">
+    
+    <body style="background-color: #ccc;">
+        <div class="text-center">
+            <h1>
+                Bicis UTEM
+            </h1>
+        </div>
+        <hr>
+        <br />
+        <div class="maincontent" style="background-color: #FFF; margin: auto; padding: 20px; width: 450px; border-top: 2px solid #27ae60;">
+            <div class="text-center">
+                <h1>Hola [nombre usuario] </h1>
+                <p>Su bicicleta ha sido retirada del sistema.</p>
+                <p>
+                    Si usted no fue el que realizo el retiro, por favor acercarse a la brevedad a portería. <b>
+                </p>
+                <p>Saluda atentamente,</p>
+                
+                <p>La Administración.
+                </p>
+            </div>
+        </div>
+    </body>
 </html>
