0d191770e024c486afc4fe8d2e46134405ca0e1f
Gorky <gorky.lazcano@ceinf.cl>
2017-01-12 16:12:51 -0400

    arreglos

--- resources/views/cliente/template/partials/nav.blade.php
+++ resources/views/cliente/template/partials/nav.blade.php
@@ -12,37 +12,26 @@
       <a class="navbar-brand" href="http://www.utem.cl/" target="_blank" title="utem.cl"><img src="{{ asset('images/UTEM.png') }}" style="width: 140%;height: 140%"></a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" align="center">
-      <ul class="nav navbar-nav">
-        <li class="dropdown">
-          <ul class="dropdown-menu">
-          </ul>
-        </li>
-      </ul><!--
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>-->
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{ route('cliente.home') }}">Home</a></li>
+        <li {{ setActive('cliente/home') }}><a href="{{ route('cliente.home') }}"><i class="fa fa-home fa-2x" aria-hidden="true" style="color: #080266;"></i>&nbsp; Home</a>
+        </li>
         @if (Auth::guest())
-            <li><a href="{{ url('/login') }}">Ingresar</a></li>
+            <li><a href="{{ url('/login') }}">Ingresar</a>
+            </li>
             <!--<li><a href="{{ url('/register') }}">Crear Cuenta</a></li>-->
         @else
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-user-circle fa-2x" aria-hidden="true" style="color: #080266;"></i>&nbsp;
                     {{ Auth::user()->name }} <span class="caret"></span>
                 </a>
 
                 <ul class="dropdown-menu" role="menu">
+                    <li><a href="{{ route('cliente.users.detalle', Auth::user()->id) }}" style="font-size:16px;" title="Ver Perfil">
                     <span class="glyphicon glyphicon-eye-open " style="color: #54CC14" aria-hidden="true" title="Ver Perfil"></span>   Ver Perfil</a></li>
                     <li><a href="{{ route('cliente.users.edit', Auth::user()->id) }}" style="font-size: 16px" title="Editar Perfil">
                       <span class="glyphicon glyphicon-pencil" aria-hidden="true" style="color: #313AFF" title="Editar Perfil"></span>   Editar Perfil</a></li>
                     <li role="separator" class="divider"></li>
-                    <li><a href="{{ url('/logout') }}" title="Cerrar Sesion" style="font-size: 16px;"><i class="fa fa-btn fa-sign-out"></i><span class="glyphicon glyphicon-exclamation-sign" style="color: #FF0000;" aria-hidden="true" title="Cerrar Sesion"></span>   Cerrar Sesion </a></li>
+                    <li><a href="{{ url('/logout') }}" title="Cerrar Sesion" style="font-size: 16px;"><span class="glyphicon glyphicon-exclamation-sign" style="color: #FF0000;" aria-hidden="true" title="Cerrar Sesion"></span>   Cerrar Sesion </a></li>
                     
                 </ul>
             </li>
---
+++ resources/views/cliente/users/detalle.blade.php
@@ -0,0 +1,141 @@
+@php
+	use App\User;
+@endphp
+
+@extends('cliente.template.main')
+
+@section('title','Detalles ' . $title)
+
+@section('content')
+		
+		<div class="row">
+			<div class="col-md-3">
+				<div class="form-group">
+					{!! Form::label('name', 'Nombre') !!}
+					{!! Form::text('name',  $user->name ,['class' => 'form-control' ,'readonly'=>'readonly']) !!}
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="form-group">
+					{!! Form::label('rut', 'Rut') !!}
+					{!! Form::text('rut', formato_rut($user->rut),['class' => 'form-control','readonly'=>'readonly']) !!}
+				</div>
+			</div>
+				<div class="col-md-4">
+					<div class="form-group">
+				@if($user->nomTipo != "Visita")
+					{!! Form::label('email', 'Correo Electronico') !!}
+					{!! Form::text('email', $user->email ,['class' => 'form-control' ,'readonly'=>'readonly']) !!}
+				@endif
+				</div>
+			</div>
+			
+		</div>
+
+		
+
+		
+		<div class="row">
+			<div class="col-md-3">
+				<div class="form-group">
+					{!! Form::label('type_name','Tipo') !!}
+					{!! Form::text('type_name',  $user->nomTipo ,['class' => 'form-control' ,'readonly'=>'readonly']) !!}
+				</div>
+			</div>
+			<div class="col-md-5">
+				@if($user->tipo == 4)
+					<div class="form-group">
+						{!! Form::label('carera_id','Carrera') !!}
+						{!! Form::text('carrera_name',  $user->nomCarrera ,['class' => 'form-control' ,'readonly'=>'readonly']) !!}
+					</div>
+				@endif
+			</div>
+		</div>
+		
+		<hr>
+		<br>
+		@if($bikes == null)
+			<li style="font-weight:bold;list-style:none;"><span class="glyphicon glyphicon-eye-open " style="color: #FF0000" aria-hidden="true"></span> Este usuario no tiene registradas bicicletas</li>
+			<br>
+		@else
+			<li style="font-weight:bold;list-style:none;"><span class="glyphicon glyphicon-eye-open " style="color: #FF0000" aria-hidden="true"></span> Bicicletas :</li>
+			<br>
+			<table class="table" width="100%" cellpadding="0" cellspacing="0" id="datatable_bicicletas">
+			<thead>
+				<th>¿Está en la Universidad ?</th>
+				<th>Descripcion</th>
+				<th>Fecha última llegada</th>
+				<th>Encargado última llegada</th>
+				<th>Fecha última salida</th>
+				<th>Encargado última salida</th>
+			</thead>
+			<tbody>
+				@foreach($bikes as $bike)
+					@php
+						$auxLlegada = 0;
+						$auxSalida = 0;
+						if($bike->encargado_a !=0){
+							$encargadoLLegada = User::find($bike->encargado_a);
+						}else{
+							$auxLlegada = 1;
+						}
+						if($bike->encargado_s !=0){
+							$encargadoSalida = User::find($bike->encargado_s);
+						}else{
+							$auxSalida = 1;
+						}
+						
+					@endphp
+					<tr>
+						<th>
+							@if($bike->activa == 1)
+								Si
+							@else
+								No
+							@endif
+						</th>
+						<th>{{ $bike->descripcion }}</th>
+						<th>{{ formato_y_m_d($bike->fecha_a) }}</th>
+						<th>
+							@if($auxLlegada == 1)
+								No hay datos
+							@else
+								{{ $encargadoLLegada->name }}
+							@endif
+						</th>
+						<th>{{ formato_y_m_d($bike->fecha_s) }}</th>
+						<th>
+							@if($auxSalida == 1)
+								No hay datos
+							@else
+								{{ $encargadoSalida->name }}
+							@endif
+						</th>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+		@endif
+
+
+		<a href="{{ url()->previous() }}" class="btn btn-primary" title="Volver"><span class="fa fa-btn fa-sign-in" aria-hidden="true" title="Volver">Volver</span></a>
+
+@endsection
+@section('script')
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#datatable_bicicletas').DataTable({
+    	dom: 'Bfrtip',
+    	stateSave: true,
+        buttons: [
+            {
+            	text: 'Ocular Columnas',
+                extend: 'colvis',
+                columns: ':not(:first-child)'
+            }
+        ]
+    });
+});
+
+</script>
+@endsection
